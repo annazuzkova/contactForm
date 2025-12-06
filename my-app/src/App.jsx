0caf7bd2e4dc64266aa14306edae4a0ac359e5b0
AnnaZuzkova <famzu2018@gmail.com>
2025-12-06 12:23:59 +0200

    local

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -17,6 +17,19 @@ class App extends React.Component {
     number: "",
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
     const newUser = {
       id: `id-${Date.now()}`,
